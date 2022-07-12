ec1d4497b4cd48c3a362b5418d500d3188a34691
dothuongton <dothuongton@gmail.com>
2022-07-13 02:54:49 +0700

    4th

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 $(document).on('ready', function () {
     $(".img-slide").slick({
         
-        dots: true,
+        dots: false,
         infinite: true,
         prevArrow: $('.prev'),
         nextArrow: $('.next'),
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay:true,
-        autoplaySpeed:700
+        autoplay:true,   
+        autoplaySpeed:5000
     });
 });
-  
+
+$(document).on('ready', function () {
+    $(".platform_slide").slick({
+        infinite:true,
+        prevArrow: $('.platform_slide-prev'),
+        nextArrow: $('.platform_slide-next'),
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+});
